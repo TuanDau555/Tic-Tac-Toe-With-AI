20c7c70a1acfc7ca41add46899355f1b5e2f82a7
TuanDau555 <nguyenmtuan505@gmail.com>
2025-06-08 12:45:13 +0700

    Win Condition
    Win condition by counting consecutive celss

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+/// <summary>
+/// /// Generic classes for the use of singletons.
+/// There are three types of singletons:
+/// 1.MonoBehaviour -> for the use of singletons to MonoBehaviours.
+/// 2.Persistent -> when we need to makde sure the object is not destroyed during the session. 
+/// </summary>
+
+
 public class Singleton<T> : MonoBehaviour where T : Component
 {
     public static T Instance { get; private set; }
